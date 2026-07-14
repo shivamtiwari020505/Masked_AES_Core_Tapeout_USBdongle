f8b66ad8e84088ab80409c14d4a1807067e4c8a5
Shiii <shivamtiwari020505@users.noreply.github.com>
2026-07-14 20:18:05 +0530

    Limit OpenLane routing to met4

--- config.tcl
+++ config.tcl
@@ -9,6 +9,7 @@ set ::env(CLOCK_PERIOD) "50"
 set ::env(MAX_FANOUT_CONSTRAINT) 8
 set ::env(SYNTH_MAX_FANOUT) 8
 set ::env(SYNTH_STRATEGY) "AREA 0"
+set ::env(RT_MAX_LAYER) "met4"
 
 # No scan insertion for this side-channel-sensitive masked datapath.
 set ::env(RUN_DFT) 0
