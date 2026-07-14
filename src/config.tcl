a4c08f68723745061db54f3d2e223b7d07fd8825
Shiii <shivamtiwari020505@users.noreply.github.com>
2026-07-14 19:45:42 +0530

    Add Tiny Tapeout source config

---
+++ src/config.tcl
@@ -0,0 +1,16 @@
+set ::env(DESIGN_NAME) tt_um_masked_aes_round_only
+set ::env(VERILOG_FILES) "\
+    $::env(DESIGN_DIR)/masked_sbox.sv \
+    $::env(DESIGN_DIR)/masked_aes_round_only.sv"
+
+set ::env(CLOCK_PORT) "clk"
+set ::env(CLOCK_PERIOD) "50"
+
+set ::env(MAX_FANOUT_CONSTRAINT) 8
+set ::env(SYNTH_MAX_FANOUT) 8
+set ::env(SYNTH_STRATEGY) "DELAY 1"
+
+# No scan insertion for this side-channel-sensitive masked datapath.
+set ::env(RUN_DFT) 0
+set ::env(DFT_INSERTION) 0
+set ::env(SCAN_INSERTION) 0
